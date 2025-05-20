9b9c6acb21b027301902b3ffd1a97e7476bc2f4b
ShashwatK <shashwatkhatod21@gmail.com>
2025-05-20 15:56:19 +0530

    Merge remote-tracking branch 'origin/main' into main

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+// @ts-ignore
+import { createRoot } from 'react-dom/client';
 
 import { Ludo } from 'containers/Ludo/Container';
 import { store } from 'state/store';
@@ -11,13 +12,15 @@ import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(
   <Provider store={store}>
     <React.StrictMode>
       <Ludo />
     </React.StrictMode>
-  </Provider>,
-  document.getElementById('root'),
+  </Provider>
 );
 
 // If you want your app to work offline and load faster, you can change
